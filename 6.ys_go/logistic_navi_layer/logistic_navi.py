d14573783f3cd30358f5b4a062a07f825f5685f8
gomen7849 <go7849@gmail.com>
2022-05-28 19:53:38 +0900

    logistic_navi_layer

---
+++ 6.ys_go/logistic_navi_layer/logistic_navi.py
@@ -0,0 +1,53 @@
+import os
+import time
+import gc
+from datetime import datetime
+import gym
+from gym.envs.registration import register
+# ------------------------------------------------------------------------------------------------------
+import pandas as pd
+from common.func_ import save, load, create_dir
+from agent.common import get_set_env, print_env_map, get_idx_direct, print_str_direct, get_path
+from agent.common import EarlyStopping
+from agent.dqn import DQN
+from env_.logistic.logistic import LogisticEnv
+from env_.logistic.common import *
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+PATH_LOCAL = './'
+PATH_INFO_BOX = PATH_LOCAL+'data/box.csv'
+PATH_INFO_OBSTACLES = PATH_LOCAL+'data/obstacles.csv'
+PATH_DATA_TRAIN = PATH_LOCAL+'data/factory_order_train.csv'
+
+
+def run_dqn():
+    gc.collect()
+    order_train = pd.read_csv(PATH_DATA_TRAIN)
+    env = LogisticEnv(10, 9, PATH_INFO_BOX, PATH_INFO_OBSTACLES)
+
+    row_str = list(order_train.iloc[0])[0]
+    items = list(set(env.NAME_ITEM) & set(row_str))
+    items.sort()
+    env.set_p_order(items)
+    env.set_route((9, 4), env.get_p_item(items[0]))
+
+    agent = DQN(env, size_input=env.height * env.width, size_output=env.num_action)
+    agent.min_epsilon = 0.1
+    env.REWARD.GOAL = 150
+    env.REWARD.OUT_GIRD = -100
+    env.REWARD.OBSTACLE = -100
+    env.REWARD.RETURN = -100
+    env.REWARD.MOVE = -0.01
+
+    q_map, reward = agent.run(10000,
+                              buffer=10000, sampling=128,
+                              size_hidden=24, epoch=10, learning_rate=0.001, interval_train=100,
+                              discount=0.9, early_stopping=EarlyStopping(ratio=50), save_result=True,
+                              greedy=0.999, run_time=2000, max_step=90)
+
+
+if __name__ == '__main__':
+    run_dqn()
+
+
